492fd7235699a3ccfbfa15ff83f52ebef6275b47
AdamArs <kasperhauzer98@gmail.com>
2018-04-30 17:48:38 +0500

    Reload

---
+++ LenguageIntegratedQuery/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Hierarchy;
+using Collection;
+
+namespace LenguageIntegratedQuery
+{
+    class Program
+    {
+        static readonly Random R = new Random();      //closed field of random number
+
+        static void Main(string[] args)
+        {
+            Sequence<Person> seq = new Sequence<Person>();
+
+            for (int i = 0; i < 1000; i++)
+                switch (R.Next() % 3) {
+                case 0: seq.Add(Student.Generate()); break;
+                case 1: seq.Add(Employee.Generate()); break;
+                case 2: seq.Add(Employee.Generate()); break;
+                }
+
+            Person a = Teacher.Generate();
+        }
+    }
+}
