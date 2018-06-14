cee8b05c98ad71247099fce93937030882c40384
Kauz <35608419+KasperHauzer@users.noreply.github.com>
2018-06-15 02:24:07 +0500

    Update Program.cs

--- ObjectOfTime/Program.cs
+++ ObjectOfTime/Program.cs
@@ -28,29 +28,6 @@ namespace ObjectOfTime
             Console.WriteLine(a1 == a2);
             Console.WriteLine(-a1 != -a2);
             Console.WriteLine();
-
-            Console.WriteLine("1-автозаполнение, 2-ручное заполнение:");
-            string ans = Console.ReadLine();
-
-            if (ans == "1") {
-                TimeArray timeArray = new TimeArray(20, new Random());
-
-                for (int i = 0; i < timeArray.Size; i++)
-                    Console.WriteLine(timeArray[i]);
-            } else {
-                TimeArray timeArray = new TimeArray(5);
-
-                for (int i = 0; i < timeArray.Size; i++){
-                    int a = 0;
-                    while (!int.TryParse(Console.ReadLine(), out a) && a < 0)
-                        Console.WriteLine("Введите целое положительное число!");
-
-                    timeArray[i] = a;
-                }
-
-                for (int i = 0; i < timeArray.Size; i++)
-                    Console.WriteLine(timeArray[i]);
-            }
         }
     }
 }
